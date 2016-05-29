34876096c2d5e5f7d6345de4c77cfe71b3360564
Braids Constance <BraidsCC@users.noreply.github.com>
2016-05-29 00:47:32 -0400

    rename tricks-player to presto-player. make player struct transparent without spamming zone contents.

--- presto/player.rkt
+++ presto/player.rkt
@@ -1,35 +1,31 @@
 #lang racket
 
 (require "../braids/util.rkt")
+(require "zone.rkt")
 
 
-(struct/provide/contract-out tricks-player
-                             ;----;
+(struct/provide/contract-out presto-player
+                             ;------------
                              ([name string?]
-                              [zones hash-eq?] ; maps zone-symbol? => zone?
-                              ))
+                              [zone-dict dict?] ; maps zone-symbol? => presto-zone?
+                              )
+                             #:transparent)
 
 (define/provide/contract-out (make-player p-name lib-card-names side-card-names)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;  -----------
-  (-> string? (listof string?) (listof string?)  tricks-player?)
-  (tricks-player p-name (make-hasheq (list (list 'library lib-card-names)
+  (-> string? (listof string?) (listof string?)  presto-player?)
+  (presto-player p-name (make-hasheq (list (list 'library lib-card-names)
                                     (list 'sideboard side-card-names)))))
 
 ;; not to be confused with player-zones, which is plural  :-/
 (define/provide/contract-out (player-zone player zone-sym)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;  -----------
-  (-> tricks-player? symbol?  zone?)
-  (hash-ref (tricks-player-zones player) zone-sym))
+  (-> presto-player? symbol?  presto-zone?)
+  (hash-ref (presto-player-zone-dict player) zone-sym))
 
 
 (define/provide/contract-out (set-player-zone! player zone-sym zone)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;  ----------------
-  (-> tricks-player? symbol? zone?  void?)
-  (hash-ref! (tricks-player-zones player) zone-sym zone)
+  (-> presto-player? symbol? presto-zone?  void?)
+  (hash-ref! (presto-player-zone-dict player) zone-sym zone)
   (void))
-
-
-(define/provide/contract-out (zone? val)
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;  -----
-  (-> any/c  boolean?)  
-  (list? val))
